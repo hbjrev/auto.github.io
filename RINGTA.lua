a786a1401bcb29ff4cd8bb80616dc8100a6df34f
hbjrev <eeeiqjjj218@gmail.com>
2025-05-05 23:16:17 -0400

    Create RINGTA.lua

---
+++ RINGTA.lua
@@ -0,0 +1,16 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+LocalPlayer.OnTeleport:Connect(function(state)
+    if state == Enum.TeleportState.InProgress then
+        if syn and syn.queue_on_teleport then
+            syn.queue_on_teleport([[ 
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/hbjrev/RINGTA.github.io/refs/heads/main/AUTOBOND.lua"))()
+            ]])
+        elseif queue_on_teleport then
+            queue_on_teleport([[ 
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/hbjrev/RINGTA.github.io/refs/heads/main/AUTOBOND.lua"))()
+            ]])
+        end
+    end
+end)
